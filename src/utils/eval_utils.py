7eb6a4b1a73b705b544770893d762936e23593bd
Sara Khosravi <45513600+sarakh1999@users.noreply.github.com>
2024-02-18 14:12:06 -0800

    Update eval_utils.py

--- src/utils/eval_utils.py
+++ src/utils/eval_utils.py
@@ -14,7 +14,7 @@ def evaluate_standard(gt_labels, pred_labels):
     # scikit-learn APIs in way they can deal with strings 
     # as label. Remeber to import the functions you use!
     print(set(gt_labels))
-    pred_labels = [d["label"] for d in pred_labels if "label" in d]
+    pred_label = [d["label"] for d in pred_labels if "label" in d]
     print(pred_label[-1])
     
     accuracy = accuracy_score(gt_labels, pred_labels["label"])
