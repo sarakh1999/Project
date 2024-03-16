7597789cdbc9973f4466cbbea69622d5cae62982
Sara Khosravi <45513600+sarakh1999@users.noreply.github.com>
2024-03-15 20:29:41 -0700

    Update batch_prompt.py for train again

--- src/phi/batch_prompt.py
+++ src/phi/batch_prompt.py
@@ -71,15 +71,15 @@ def batch_prompt(model, tokenizer, annotations_filepath, output_filepath, prompt
         dump_jsonl(output_data, output_filepath)
 
     else:
-        # print("HIII")
-        # dump_jsonl(output_data, output_filepath)
+        print("HIII")
+        dump_jsonl(output_data, output_filepath)
         print(len(output_data))
     
         # Save the predictions to a text file
-        output_filepath = "fof_predictions.txt"  # Set the output filepath to the required filename
-        with open(output_filepath, "w") as f:
-           for prediction in output_data:
-               f.write(f"{prediction}\n")
+        # output_filepath = "fof_predictions.txt"  # Set the output filepath to the required filename
+        # with open(output_filepath, "w") as f:
+        #    for prediction in output_data:
+        #        f.write(f"{prediction}\n")
 
 def main(args):
     model, tokenizer = model_and_tokenizer_setup(args.model_id_or_path)
