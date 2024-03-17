5becdc0b2c2fcfb6b7ba0de2ec114b00973b4d99
Sara Khosravi <45513600+sarakh1999@users.noreply.github.com>
2024-03-16 23:29:27 -0700

    Update model_setup.py

--- src/phi/phi_utils/model_setup.py
+++ src/phi/phi_utils/model_setup.py
@@ -84,8 +84,7 @@ def model_and_tokenizer_setup(model_id_or_path):
                                                 model_name,
                                                 quantization_config=bnb_config,
                                                 #torch_dtype=torch.float16,
-                                                device_map="auto",
-                                            )
+                                                device_map="auto",)
 
     # Load the tokenizer with left padding
     tokenizer = AutoTokenizer.from_pretrained(model_id_or_path, 
